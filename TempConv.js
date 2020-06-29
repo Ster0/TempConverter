007f7ce5642b36dfa90aa680554b4d446f47ce0b
Stero <robatelsteve@hotmail.com>
2020-06-29 20:01:56 +0200

    temp converter

---
+++ TempConv.js
@@ -0,0 +1,6 @@
+//(Celsius * 9/5) + 32 = Farenheit.
+
+var celsius = 21;
+var farenheit = (celsius * 9/5) + 32;
+
+console.log(farenheit);
---
+++ Temp_conv.py
@@ -0,0 +1,6 @@
+#(Celsius * 9/5) + 32 = Farenheit
+
+Celsius = 21
+Farenheit = (Celsius * 9/5) + 32
+
+print(Farenheit)
